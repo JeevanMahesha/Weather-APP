81b7066336723bd0c7f0db12686045ebccefabec
Jeevan-M <csejeevanm@gmail.com>
2020-05-31 13:01:04 +0530

    setup finished for heroku deployement

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ const locationtime = document.querySelector('#locationtime')
 
 weatherform.addEventListener('submit', (e) => {
     e.preventDefault()
-    fetch('http://localhost:3000/weather?address=' + searchvalue.value)
+    fetch('/weather?address=' + searchvalue.value)
         .then((res) => res.json())
         .then((data) => {
             if (data.Error) {
